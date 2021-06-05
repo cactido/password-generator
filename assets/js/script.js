46b10a0db08fa1d3b682c6fab58e00d2c99f80eb
Travis Russell <mtrussell@gmail.com>
2021-06-05 17:12:45 -0600

    added code to generate at least one of each requested type of character

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ function generatePassword() {
   var passwordLength = 0;
   var splitPassword = [];
 
+  /*
   while (passwordLength < 8 || passwordLength > 128) {
     var passwordLength = prompt('Choose a password length (8 - 128 characters)');
   }
@@ -19,21 +20,58 @@ function generatePassword() {
       break;
     }
   }
+  */
 
-  let alphabet = ['a','b','c,','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var passwordLowerCase = true;
+  var passwordUpperCase = true;
+  var passwordNumbers = true;
+  var passwordSpecialCharacters = true;
+  var passwordLength = 24;
+
+  let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   let specialCharacters = ['!','#','$','%','&','(',')','*','+','-',':',';','?','_','^','~'];
 
   var currentCharacter = '';
+  var randomPosition = 0;
 
-  if (passwordLowerCase = true) {
+  if (passwordLowerCase == true) {
     currentCharacter = alphabet[Math.floor(Math.random() * alphabet.length)];
     splitPassword.push(currentCharacter);
     passwordLength--;
   }
 
-  for (let i = 0; i < passwordLength; i++) {
-    
+  if (passwordUpperCase == true) {
+    currentCharacter = alphabet[Math.floor(Math.random() * (alphabet.length))];
+    currentCharacter = currentCharacter.toUpperCase();
+    randomPosition = Math.floor(Math.random() * (splitPassword.length + 1));
+    splitPassword.splice(randomPosition,0,currentCharacter);
+    passwordLength--;
   }
+  
+  if (passwordNumbers == true) {
+    currentCharacter = Math.floor(Math.random() * 10);
+    randomPosition = Math.floor(Math.random() * (splitPassword.length + 1));
+    splitPassword.splice(randomPosition,0,currentCharacter);
+    passwordLength--;
+  }
+
+  if (passwordSpecialCharacters == true) {
+    currentCharacter = specialCharacters[Math.floor(Math.random() * (specialCharacters.length))];
+    randomPosition = Math.floor(Math.random() * (splitPassword.length + 1));
+    splitPassword.splice(randomPosition,0,currentCharacter);
+    passwordLength--;
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    characterSet = Math.floor(Math.random() * 3);
+    if characterSet == 0 {
+      
+    }
+  }
+
+  console.log(splitPassword.length);
+  console.log(splitPassword.join(''));
+  return splitPassword.join('');
 }
 
 // Get references to the #generate element
